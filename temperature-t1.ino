fc9eafb12658c774de35912a5011bc5f345d1dab
Chris Todorov <chris.todorov@gmail.com>
2020-04-14 23:42:28 -0700

    Add support for reading temperature from two sensors

--- temperature-t1.ino
+++ temperature-t1.ino
@@ -16,19 +16,20 @@ int led = D7;
 DallasTemperature dallas(new OneWire(temperatureSensor));
 DeviceAddress thermometerAddress;
 
-double temperature;
 int resolution;
 int numberOfDevices;
+
+#define DALLAS_RESOLUTION 12
 
 void setup(){
     pinMode(led, OUTPUT);
     
-    temperature = 0;
     resolution = 0;
+    numberOfDevices = 0;
     
     dallas.begin();
     
-    dallas.setResolution(12);
+    dallas.setResolution(DALLAS_RESOLUTION);
     
     resolution = dallas.getResolution(thermometerAddress);
     numberOfDevices = dallas.getDeviceCount();
@@ -40,20 +41,39 @@ void loop(){
     
     dallas.requestTemperatures();
     
-    float temp_temperature = dallas.getTempC(thermometerAddress);
+    double temperatures[numberOfDevices] = { };
     
-    if (temp_temperature != DEVICE_DISCONNECTED_C) {
-        temperature = temp_temperature;
+    // Loop through each device and build a payload of the data.
+    for (int i = 0; i < numberOfDevices; i++) {
+        // Search the wire for address
+        if (dallas.getAddress(thermometerAddress, i)){
+    		// Output the device ID
+    		Serial.print("Temperature for device: ");
+    		Serial.println(i, DEC);
+
+            float temp_temperature = dallas.getTempC(thermometerAddress);
+
+            if (temp_temperature != DEVICE_DISCONNECTED_C) {
+                temperatures[i]= temp_temperature;
+                 Serial.print("Temp C: ");
+                 Serial.print(temp_temperature);
+                 Serial.print(" Temp F: ");
+                 Serial.println(DallasTemperature::toFahrenheit(temp_temperature));
+            }
+        }
     }
     
-    Particle.variable("temperature", &temperature, DOUBLE);
+    Particle.variable("temperature1", &temperatures[0], DOUBLE);
+    Particle.variable("temperature2", &temperatures[1], DOUBLE);
     Particle.variable("resolution", &resolution, INT);
+    Particle.variable("devices", &numberOfDevices, INT);
     
     String data = String::format(
-        "{ \"tags\" : {\"id\": \"%s\", \"location\": \"%s\"}, \"values\": {\"temperature\": %f, \"devices:\": %d } }",
+        "{ \"tags\" : {\"id\": \"%s\", \"location\": \"%s\"}, \"values\": {\"temperature_1\": %f, \"temperature_2\": %f, \"devices:\": %d } }",
         "t2",
         "fermentor 1",
-        temperature,
+        temperatures[0],
+        temperatures[1],
         numberOfDevices
     );
     
