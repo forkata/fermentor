87ad0e964f4150f2b2252763cbd92daa7672a541
Chris Todorov <chris.todorov [at] gmail.com>
2021-10-19 13:22:43 -0700

    Fix spacing in firmware
    This was written in the Particle Web IDE so the spacing is all out of
    wack. I'll paste this back in there and hopefully it stays this way.

--- temperature.ino
+++ temperature.ino
@@ -22,81 +22,81 @@ int numberOfDevices;
 #define DALLAS_RESOLUTION 12
 
 void setup(){
-    pinMode(led, OUTPUT);
+  pinMode(led, OUTPUT);
 
-    resolution = 0;
-    numberOfDevices = 0;
+  resolution = 0;
+  numberOfDevices = 0;
 
-    dallas.begin();
+  dallas.begin();
 
-    dallas.setResolution(DALLAS_RESOLUTION);
+  dallas.setResolution(DALLAS_RESOLUTION);
 
-    resolution = dallas.getResolution(thermometerAddress);
-    numberOfDevices = dallas.getDeviceCount();
+  resolution = dallas.getResolution(thermometerAddress);
+  numberOfDevices = dallas.getDeviceCount();
 }
 
 void loop(){
-    // Turn LED On
-    digitalWrite(led, HIGH);
+  // Turn LED On
+  digitalWrite(led, HIGH);
 
-    dallas.requestTemperatures();
+  dallas.requestTemperatures();
 
-    double temperatures[numberOfDevices] = { };
+  double temperatures[numberOfDevices] = { };
 
-    String values = String("{");
+  String values = String("{");
 
-    // Loop through each device and build a payload of the data.
-    for (int i = 0; i < numberOfDevices; i++) {
-        // Search the wire for address
-        if (dallas.getAddress(thermometerAddress, i)){
-    		// Output the device ID
-    		Serial.print("Temperature for device: ");
-    		Serial.println(i, DEC);
+  // Loop through each device and build a payload of the data.
+  for (int i = 0; i < numberOfDevices; i++) {
+    // Search the wire for address
+    if (dallas.getAddress(thermometerAddress, i)){
+      // Output the device ID
+      Serial.print("Temperature for device: ");
+      Serial.println(i, DEC);
 
-            float tempTemperature = dallas.getTempC(thermometerAddress);
-            int sensorNumber = i + 1;
+      float tempTemperature = dallas.getTempC(thermometerAddress);
+      int sensorNumber = i + 1;
 
-            if (tempTemperature != DEVICE_DISCONNECTED_C) {
-                temperatures[i]= tempTemperature;
-                values = String::format(
-                    "%s \"temperature_%d\": %f, ",
-                    values.c_str(),
-                    sensorNumber,
-                    tempTemperature
-                );
+      if (tempTemperature != DEVICE_DISCONNECTED_C) {
+        temperatures[i]= tempTemperature;
+        values = String::format(
+          "%s \"temperature_%d\": %f, ",
+          values.c_str(),
+          sensorNumber,
+          tempTemperature
+        );
 
-                String variableName = "temperature" + String(sensorNumber);
-                Particle.variable(variableName, &temperatures[i], DOUBLE);
+        String variableName = "temperature" + String(sensorNumber);
+        Particle.variable(variableName, &temperatures[i], DOUBLE);
 
-                Serial.print("Temp C: ");
-                Serial.print(tempTemperature);
-                Serial.print(" Temp F: ");
-                Serial.println(DallasTemperature::toFahrenheit(tempTemperature));
-            }
-        }
+        Serial.print("Temp C: ");
+        Serial.print(tempTemperature);
+        Serial.print(" Temp F: ");
+        Serial.println(DallasTemperature::toFahrenheit(tempTemperature));
+      }
     }
+  }
 
-    values = String::format(
-        "%s \"devices:\": %d }",
-        values.c_str(),
-        numberOfDevices
-    );
+  values = String::format(
+    "%s \"devices:\": %d }",
+    values.c_str(),
+    numberOfDevices
+  );
 
-    Particle.variable("resolution", &resolution, INT);
-    Particle.variable("devices", &numberOfDevices, INT);
+  Particle.variable("resolution", &resolution, INT);
+  Particle.variable("devices", &numberOfDevices, INT);
 
-    String data = String::format(
-        "{ \"tags\" : {\"id\": \"%s\", \"location\": \"%s\"}, \"values\": %s }",
-        "t2",
-        "fermentor 1",
-        values.c_str()
-    );
+  String data = String::format(
+    "{ \"tags\" : {\"id\": \"%s\", \"location\": \"%s\"}, \"values\": %s }",
+    "t2",
+    "fermentor 1",
+    values.c_str()
+  );
 
-    Particle.publish("temperature", data, PRIVATE);
+  Particle.publish("temperature", data, PRIVATE);
 
-    // Turn the LED Off
-    digitalWrite(led, LOW);
+  // Turn the LED Off
+  digitalWrite(led, LOW);
 
-    // Wait 60 sec before repeating
-    delay(60000);
+  // Wait 60 sec before repeating
+  delay(60000);
 }
